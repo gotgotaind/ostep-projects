0efb0d56a8e5265b4e4bc71d0f4ca14bc63d467e
gotgotaind <gotgotaind@gmail.com>
2021-04-28 00:12:48 +0200

    seems to print one file fine.

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -6,9 +6,18 @@
 int main( int argc, char * argv[] ) {
     char * fn = argv[1];
     FILE *fp = fopen(fn, "r");
+    int bs=16000;
+
     if (fp == NULL) {
         printf("cannot open file %s : %s\n",fn,strerror(errno));
         exit(1);
     }
+
+    char buf[bs];
+    while ( fgets(buf,bs,fp) != NULL ) {
+        printf("%s",buf);
+    }
+    printf("\n");
+
 
 }
