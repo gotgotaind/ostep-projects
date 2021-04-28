e52ad7df7ebd7d97c1f0088b4fb7d924e6cdb448
gotgotaind <gotgotaind@gmail.com>
2021-04-28 22:09:39 +0200

    Seems to work as expected, even with several files as argument

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -3,21 +3,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main( int argc, char * argv[] ) {
-    char * fn = argv[1];
-    FILE *fp = fopen(fn, "r");
-    int bs=16000;
-
-    if (fp == NULL) {
-        printf("cannot open file %s : %s\n",fn,strerror(errno));
-        exit(1);
-    }
-
+int main(int argc, char *argv[])
+{
+    char *fn;
+    FILE *fp;
+    int bs = 16000;
     char buf[bs];
-    while ( fgets(buf,bs,fp) != NULL ) {
-        printf("%s",buf);
+
+    // That's the spec
+    if (argc == 1)
+        exit(0);
+
+    int i = 1;
+    while (argv[i] != NULL)
+    {
+        fn = argv[i];
+        fp = fopen(fn, "r");
+
+        if (fp == NULL)
+        {
+            printf("wcat: cannot open file\n");
+            // better error message, but the specs are to print as in previous line
+            // printf("cannot open file %s : %s\n",fn,strerror(errno));
+            exit(1);
+        }
+
+        
+        while (fgets(buf, bs, fp) != NULL)
+        {
+            printf("%s", buf);
+        }
+        //printf("\n");
+        i++;
+
     }
-    printf("\n");
-
-
+    exit(0);
 }
