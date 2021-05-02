3e2667634589b253fd17c1699c033119510f64ce
gotgotaind <gotgotaind@gmail.com>
2021-05-02 23:06:16 +0200

    Getting unexpected results...

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +12,8 @@ int main(int argc, char *argv[])
     ssize_t nread;
     char *fn;
 
+    // printf(argv[0]);
+
     // That's the spec
     if (argc == 1) 
     {
@@ -18,7 +21,10 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
-    int input_file_i = 1;
+    char * pattern = argv[1];
+    size_t pattern_length=strlen(pattern);
+
+    int input_file_i = 2;
     while (argv[input_file_i] != NULL)
     {
         fn = argv[input_file_i];
@@ -28,18 +34,33 @@ int main(int argc, char *argv[])
         {
             printf("wgrep: cannot open file\n");
             // better error message, but the specs are to print as in previous line
-            // printf("cannot open file %s : %s\n",fn,strerror(errno));
+            printf("cannot open file %s : %s\n",fn,strerror(errno));
             exit(1);
         }
 
-        
+        int line_nb=0;
         while ((nread = getline(&line, &len, stream)) != -1) {
             // printf("Retrieved line of length %zd:\n", nread);
             // fwrite(line, nread, 1, stdout);
+            int char_matched_count=0;
+            bool line_matched=false;
+            
             for( int i = 0; i < ( len ) ; i++ ) {
                 // fputc(line[i],stdout);
-                printf("%d,%c",i,line[i]);
+                // printf("%d,%c",i,line[i]);
+                if( line[i] == pattern[char_matched_count] ) {
+                    char_matched_count++;
+                    if( char_matched_count == pattern_length ) {
+                        line_matched=true;
+                        break;
+                    }
+                }
             }
+            // printf("%d\n",line_nb);
+            if( line_matched ) {
+                printf("%d:%s",line_nb,line);
+            }
+            line_nb++;
 
         }
 
