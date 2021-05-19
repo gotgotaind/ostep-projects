e998d61a0e997ca35309fa788a3101b82cf65f97
gotgotaind <gotgotaind@gmail.com>
2021-05-19 08:31:14 +0200

    started working on wunzip

---
+++ initial-utilities/wunzip/wunzip.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+void print_rl_and_char(uint32_t * rl, unsigned char * cc) {
+    size_t fw=1;
+    // printf("HERE\n");
+    // write the running length
+    fw=fwrite(rl, sizeof(*rl), 1,stdout);
+    // printf("%d",*rl);
+    if( fw != 1 ) {
+        printf("wzip: failed to fwrite rl\n");
+        exit(1);
+    }
+
+    // write the char
+    fw=fwrite(cc, sizeof(*cc), 1,stdout);
+    // printf("%c\n",*cc);
+    if( fw != 1 ) {
+        printf("wzip: failed to fwrite cc\n");
+        exit(1);
+    }
+}
+
+void wunzip(FILE *stream)
+{
+    struct ele {
+    // running length
+    uint32_t rl;
+    // current char
+    unsigned char cc;
+    };
+
+    size_t bs = 16;
+    size_t nread;
+    size_t total_read=0;
+    ele buf[bs];
+
+    // printf("in wzip\n");
+
+    if (stream == NULL)
+    {
+        printf("wunzip: cannot open file\n");
+        // better error message, but the specs are to print as in previous line
+        // printf("cannot open file %s : %s\n",fn,strerror(errno));
+        exit(1);
+    }
+
+
+
+    while ( (ferror(stream) == 0) && (feof(stream) == 0) )
+    {
+        nread=fread(buf, sizeof(unsigned char), bs, stream);
+        // printf("in while fread\n");
+        for(int i=0;i<nread;i++) {
+            if( *first_char ) {
+                *rl=0;
+                *cc=buf[i];
+                *first_char=false;
+            }
+
+            if( *cc == buf[i] ) {
+                *rl=*rl+1;
+                // if we reached the end of stream and it's the last character we've read
+                // print the char and its running length
+                // if( (feof(stream) != 0) && (i==(nread-1)) && last_file) {
+                // printf("Last file : %s\n",last_file ? "true" : "false");
+                // printf("feof : %s\n", (feof(stream) != 0) ? "true" : "false");
+                // printf("i : %d,nread-1 : %zu\n",i,(nread-1));
+                if( (feof(stream) != 0) && (i==(nread-1)) && last_file) {
+                    
+                    print_rl_and_char(rl,cc);
+                }
+            }
+            else
+            {
+                // printf("Last file : %s\n",last_file ? "true" : "false");
+                // printf("feof : %s\n", (feof(stream) != 0) ? "true" : "false");
+                // printf("i : %d,nread-1 : %zu\n",i,(nread-1));
+                print_rl_and_char(rl,cc);
+                *cc = buf[i];
+                *rl=1;
+            }
+            
+            // printf("%c",buf[i]);
+        }
+        
+        total_read=total_read+nread;
+    }
+
+    if( ferror(stream) != 0 ) {
+        printf("wzip: error during fread\n");
+        exit(1);
+    }
+    if( feof(stream) == 0 ) {
+        printf("wzip: fread stopped reading the file while feof was not reach and ferror was not raised.\n");
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *stream;
+    char *fn;
+
+    // That's the spec
+    if (argc == 1)
+    {
+        printf("wunzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+
+    int input_file_i = 1;
+    while (argv[input_file_i] != NULL)
+    {
+        fn = argv[input_file_i];
+        stream = fopen(fn, "r");
+        wunzip(stream);
+        input_file_i++;
+    }
+    
+    exit(0);
+}
